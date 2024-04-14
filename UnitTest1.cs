6f2be5eb5eb08b0f588b67f0c490734246fe8084
IgromanYTS <83410566+IgromanYTS@users.noreply.github.com>
2024-04-14 22:41:53 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calc.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void expression_sum2and2_returned_PostfixForm_plus()
+        {
+            string expression = "2+2";
+            string expected = "2 2 +";
+            string result = Program.InfixToPostfix(expression);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_sum2and2_returned_PostfixForm_multiply()
+        {
+            string expression = "2*2";
+            string expected = "2 2 *";
+            string result = Program.InfixToPostfix(expression);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_sum2and2_returned4()
+        {
+            string expression = "2*2";
+            int expected = 4;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_brack_sum2and2_brack_andmultiply_returned8()
+        {
+            string expression = "(2+2)*2";
+            int expected = 8;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_sum2and2_andmultiply_returned6()
+        {
+            string expression = "2+2*2";
+            int expected = 6;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_brack_sum2and2_multiply2_brack_andmultiply_returned12()
+        {
+            string expression = "(2+2*2)*2";
+            int expected = 12;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_brack_sum2and2_brack_andmultiply_returnedERROR()
+        {
+            string expression = "(2+2)*2";
+            int expected = 8;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            if (expected != result)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        public void expression_brack_min2and2_brack_andmultiply2_returned0()
+        {
+            string expression = "(2-2)*2";
+            int expected = 0;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void expression_sum2and3_returnedIsNotNull()
+        {
+            string expression = "2+3";
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void expression_sum2and3_returnedAreNotEqual()
+        {
+            string expression = "2+3";
+            int test = 3;
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreNotEqual(test, result);
+        }
+        [DataTestMethod]
+        [DataRow("(2+2)*2", 8)]
+        public void Test(string expression, int expected) 
+        {
+            int result = Program.Evaluate(Program.InfixToPostfix(expression));
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ calc2.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static bool TryConvertInfixToPostfixe(string expression, out string postfixExpression)
+    {
+        postfixExpression = InfixToPostfix(expression);
+        return postfixExpression != null;
+    }
+
+    static int Main(string[] args)
+    {
+        Console.WriteLine("Please, write your example:");
+        string expression = Console.ReadLine();
+
+        //string postfixExpression = InfixToPostfix(expression);
+        if (TryConvertInfixToPostfixe(expression, out var postfixExpression))
+        {
+            int result = Evaluate(postfixExpression);
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();            
+            return 0;
+        }
+        else
+        {
+            Console.WriteLine("Error");
+            Console.ReadLine();
+            return 1;
+        }
+        
+    }
+
+    public static string InfixToPostfix(string expression)
+    {
+        int length = expression.Length;
+        Stack<string> operation = new Stack<string>();
+        Queue<string> numb_oper = new Queue<string>();
+        int i = 0;
+        int number;
+        char bracket = '(';
+        char plus = '+';
+        char minus = '-';
+        char multiply = '*';
+        char bracket_r = ')';
+        while (i < length)
+        {
+            string currentobject = expression[i].ToString();
+            if (int.TryParse(currentobject, out number))
+            {
+                numb_oper.Enqueue(currentobject);
+            }
+            else
+            {
+                if (operation.Count == 0 || operation.Peek() == bracket.ToString())
+                {
+                    operation.Push(currentobject);
+                }
+                else
+                {
+                    if ((operation.Peek() == plus.ToString() || operation.Peek() == minus.ToString()) && (currentobject == multiply.ToString()))
+                    {
+                        operation.Push(currentobject);
+                    }
+                    else if (currentobject == bracket.ToString())
+                    {
+                        operation.Push(currentobject);
+                    }
+                    else if (currentobject == bracket_r.ToString())
+                    {
+                        while (operation.Peek() != bracket.ToString())
+                        {
+                            numb_oper.Enqueue(operation.Pop());
+                        }
+                        operation.Pop();
+                    }
+                    else
+                    {
+                        while (operation.Count > 0 && operation.Peek() != bracket.ToString() && GetPriority(operation.Peek()) >= GetPriority(currentobject))
+                        {
+                            numb_oper.Enqueue(operation.Pop());
+                        }
+                        operation.Push(currentobject);
+                    }
+                }
+            }
+            i++;
+        }
+
+        while (operation.Count > 0)
+        {
+            numb_oper.Enqueue(operation.Pop());
+        }
+
+        string postfixExpression = string.Join(" ", numb_oper);
+        return postfixExpression;
+    }
+
+    public static int Evaluate(string expression)
+    {
+        string[] tokens = expression.Split(' ');
+        Stack<int> stack = new Stack<int>();
+
+        foreach (string token in tokens)
+        {
+            if (int.TryParse(token, out int number))
+            {
+                stack.Push(number);
+            }
+            else
+            {
+                int operand2 = stack.Pop();
+                int operand1 = stack.Pop();
+
+                switch (token)
+                {
+                    case "+":
+                        stack.Push(operand1 + operand2);
+                        break;
+                    case "-":
+                        stack.Push(operand1 - operand2);
+                        break;
+                    case "*":
+                        stack.Push(operand1 * operand2);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid token: " + token);
+                }
+            }
+        }
+
+        return stack.Pop();
+    }
+
+    static int GetPriority(string op)
+    {
+        switch (op)
+        {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+}
