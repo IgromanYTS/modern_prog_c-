17ebf7218f59823afe1bfeb98aa0c91d683aa5b8
IgromanYTS <83410566+IgromanYTS@users.noreply.github.com>
2024-04-18 21:24:07 +0300

    Add files via upload

---
+++ lab2.Tests/sumTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab2.Tests
+{
+    public class sum2sum2returned4
+    {
+        [TestMethod]
+        public void StrategyAdd_sum2and2_returned4()
+        {
+            Strategy strategy = new StrategyAdd();
+            int result = strategy.execute(2, 2);
+            Assert.AreEqual(4, result);
+
+        }
+    }
+}
---
+++ lab2/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Context
+    {
+        private Strategy strategy;
+        public void setStrategy(Strategy strategy)
+        {
+            this.strategy = strategy;
+        }
+        public int executeStrategy(int a, int b)
+        {
+            return strategy.execute(a, b);
+        }
+
+    }
+}
